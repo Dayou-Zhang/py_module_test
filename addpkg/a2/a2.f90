2d7283efcf25aec79f64eab7c64d106275cfb603
Dayou Zhang <zhan6350@umn.edu>
2023-09-03 02:19:23 -0500

    test blas

--- addpkg/a2/a2.f90
+++ addpkg/a2/a2.f90
@@ -1,6 +1,20 @@
-subroutine add(a, b, c)
+subroutine blas()
     implicit none
-    real, intent(in) :: a, b
-    real, intent(out) :: c
-    c = a + b
-end subroutine add
+    integer, parameter :: n = 3
+    real*8, dimension(n,n) :: A = reshape([1,2,3,2,5,6,3,6,9], [n,n])
+    real*8, dimension(n) :: w
+    integer :: info
+    real*8, dimension(3*n-1) :: work
+    integer :: lwork = 3*n-1
+
+    call dsyev('V', 'U', n, A, n, w, work, lwork, info)
+
+    if (info == 0) then
+        print *, "Eigenvalues:", w
+        print *, "Eigenvectors:"
+        print *, A
+    else
+        print *, "An error occurred:", info
+    end if
+
+end subroutine blas
--- addpkg/addlib.py
+++ addpkg/addlib.py
@@ -3,7 +3,7 @@ import addpkg.a2.a2 as a2
 
 def test():
     print(a1.add(1, 2))
-    print(a2.add(1, 2))
+    print(a2.blas())
 
 if __name__ == '__main__':
     test()
